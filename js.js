207b2d45c2a40bdc36db57409b4e77e13090b56a
JAY <+>
2017-12-20 19:50:11 +0200

    part-two

---
+++ js.js
@@ -0,0 +1,10 @@
+
+    jQuery('.slider').slick ({
+        dots: true,
+        arrows:false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 1500,
+        pauseOnDotsHover: true,
+        draggable: false
+    });
